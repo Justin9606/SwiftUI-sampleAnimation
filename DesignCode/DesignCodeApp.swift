e31fc9ae65c2c3052b27673b8884e4b4c1ce8c47
justin <tonybek.96@gmail.com>
2023-11-04 18:04:16 +0900

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 쟈니 on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
