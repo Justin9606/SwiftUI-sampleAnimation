d91c4eee8db376819e80140a0ab90416e1860355
justin <tonybek.96@gmail.com>
2023-11-06 02:33:58 +0900

    small changes

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -7,18 +7,27 @@
 
 import SwiftUI
 
+// Animation States
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        @State var show = false
+        var body: some View {
+                VStack {
+                        Text("View more")
+                                .bold()
+                                .foregroundStyle(Color.white)
+                }
+                .frame(width: show ? 320 : 300, height: show ? 600 : 60)
+                .background(Color.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .shadow(color: Color.blue.opacity(0.5), radius: 20)
+                .onTapGesture {
+                        withAnimation(.bouncy()) {
+                                show.toggle()
+                        }
+                }
         }
-        .padding()
-    }
 }
 
 #Preview {
-    ContentView()
+        ContentView()
 }
